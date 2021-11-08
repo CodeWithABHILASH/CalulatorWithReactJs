1e6f191e167752d7b36d6717a869749b973e2791
sbiswas2209 <sbiswas2209@outlook.com>
2021-11-08 12:27:54 +0530

    ✨ Add sum of two numbers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CustomInputField from "./components/CustomInputField";
+import { useState } from "react";
 
 function App() {
+  const [a, setA] = useState(null);
+  const [b, setB] = useState(null);
+  const [sum, setSum] = useState(null);
+  function calculateSum(event) {
+    event.preventDefault()
+    if(typeof(a) !== "number" || typeof(b) !== "number"){
+      alert("Fill both numbers")
+    }
+    else{
+      setSum(a+b)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={(event) => calculateSum(event)}>
+        <CustomInputField
+          label="Enter First Number"
+          onChanged={(element) => setA(parseFloat(element.target.value))}
+        />
+        <CustomInputField
+          label="Enter Second Number"
+          onChanged={(element) => setB(parseFloat(element.target.value))}
+        />
+        <button type="submit">Calculate Sum</button>
+      </form>
+      {sum != null ? <p>{sum}</p> : <div></div>}
     </div>
   );
 }
